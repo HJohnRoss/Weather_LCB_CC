e6a845cda4b3418211a9124d71c999de8a1f5ac0
John Ross <rossjohn936@gmail.com>
2025-02-24 13:57:47 -0800

    deleting logs

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const setName = () => {
 	let CC_NAME = document.querySelector('.location')
 	if(CC_NAME && CC_NAME.innerHTML != "Carson City") {
 		CC_NAME.innerHTML = "Carson City"
-		console.log("updating")	
 	}
 	
 }
